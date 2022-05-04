8127cdf29d29dfa6ebbd7091daacc1b278595341
Romina <romina.lopez@estudiantes.utec.edu.uy>
2022-05-04 10:20:53 -0300

    header

---
+++ views/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Te lo llevo</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <style type="text/css">
+        a{
+            font-family: 'Sen', sans-serif;
+            color: #fa7f72;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+
+    <img src="te_lo_llevo-removebg-preview.png" alt="Logo" class="img">
+
+    <ul>
+        <li>
+            <u><a>Comprar</a></u>
+        </li>
+        <label style="color: #389393">
+            |
+        </label>
+        <li>
+            <a>Viajar</a>
+        </li>
+    </ul>
+    </nav>
+</body>
+</html>
