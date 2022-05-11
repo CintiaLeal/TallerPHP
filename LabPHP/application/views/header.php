64e7a04ac92b6a3ed618304022ea8180d24cf676
Romina <romina.lopez@estudiantes.utec.edu.uy>
2022-05-10 22:16:17 -0300

    no logro poner el boton de busqueda a la derecha de la barra >:(

--- LabPHP/application/views/header.php
+++ LabPHP/application/views/header.php
@@ -6,46 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Te lo llevo</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    
-    
-    <style type="text/css">
-        @import url('https://fonts.googleapis.com/css2?family=Sen&family=Unica+One&display=swap');
-        a{
-            font-family: 'Sen', sans-serif;
-            color: #fa7f72;
-        }
-        .conteiner {
-            display: flex;
-            flex-wrap: wrap;
-        }
-    
-        .c1 {
-            width: 20%;
-            padding: 1%;
-        }
-        
-        .c2 {
-            width: 20%;
-            padding: 1%;
-            margin: 3%;
-        }
-    </style>
+	<link rel="stylesheet" href="/application/views/estilos.css">
+
 </head>
 <body>
-    <nav>
-        <div class="conteiner">
-            <div class="c1">
-                <img src="te_lo_llevo-removebg-preview.png" alt="Logo" height="100px" width="auto">
-            </div>
-            <div class="c2">
-                <u><a>Comprar</a></u>
-            </div>
-            <div class="c2">
-                <label style="color: #389393">|</label>
-            </div>
-            <div class="c2">
-                <u><a>Viajar</a></u>
-            </div>  
-        </div>
-    </nav>
+<nav>
+	<div class="conteiner">
+		<div class="c1">
+			<img src="te_lo_llevo-removebg-preview.png" alt="Logo" height="80px" width="auto">
+		</div>
+		<div class="c2">
+			<u><a>Comprar</a></u>
+			<label style="color: #389393">|</label>
+			<u><a>Viajar</a></u>
+		</div>
+		<div class="c1">
+			<div class="form-outline">
+				<i class="bi bi-search"></i>
+				<input type="search" id="form1" class="form-control" placeholder="Buscar ">
+			</div>
+		</div>
+		<div class="c2">
+			<button type="button" style="background-color: #389393; color: white; border-radius: 5px;">Iniciar sesión</button>
+			<button type="button" style="background-color:lightsalmon; color: white; border-radius: 5px;">Registrarse</button>
+		</div>
+	</div>
+</nav>
