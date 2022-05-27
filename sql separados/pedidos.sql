3cab410117287db5da0c1b73ce7bbb740c373f99
Romina <romina.lopez@estudiantes.utec.edu.uy>
2022-05-27 14:19:39 -0300

    pedidos y ofertas .sql actualizados

--- sql separados/pedidos.sql
+++ sql separados/pedidos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 19-05-2022 a las 14:34:07
+-- Tiempo de generación: 27-05-2022 a las 17:18:24
 -- Versión del servidor: 5.7.24
 -- Versión de PHP: 8.0.1
 
@@ -34,12 +34,22 @@ CREATE TABLE `pedidos` (
   `precio` int(11) NOT NULL,
   `imagen` varchar(256) NOT NULL,
   `link` varchar(256) DEFAULT NULL,
-  `fecha_min` varchar(10) NOT NULL,
-  `fecha_max` varchar(10) NOT NULL,
+  `fecha_min` date NOT NULL,
+  `fecha_max` date NOT NULL,
   `origen` int(11) NOT NULL,
   `destino` int(11) NOT NULL,
-  `estado` enum('activo','recibido','transito','pendiente') NOT NULL DEFAULT 'activo'
+  `estado` enum('activo','recibido','transito','pendiente') NOT NULL DEFAULT 'activo',
+  `usuario` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`numero`, `titulo`, `descripcion`, `precio`, `imagen`, `link`, `fecha_min`, `fecha_max`, `origen`, `destino`, `estado`, `usuario`) VALUES
+(16, 'cafe', 'cafe', 250, 'https://res.cloudinary.com/dmc55ugqh/image/upload/v1653662408/wewq8yeyznfsz5seb1pi.png', '', '2022-05-27', '2022-06-03', 173976, 606452, 'activo', 31),
+(17, 'cafe', 'cafe', 250, 'https://res.cloudinary.com/dmc55ugqh/image/upload/v1653662501/el7qsmrebnbuf7wt4vka.png', '', '2022-05-27', '2022-06-03', 132518, 769129, 'activo', 31),
+(18, 'cafe', 'cafecito', 250, 'https://res.cloudinary.com/dmc55ugqh/image/upload/v1653663523/a5wky89436nksfmfn7wo.png', '', '2022-05-27', '2022-06-03', 606538, 132960, 'activo', 33);
 
 --
 -- Índices para tablas volcadas
@@ -50,7 +60,10 @@ CREATE TABLE `pedidos` (
 --
 ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`numero`),
-  ADD UNIQUE KEY `numero` (`numero`);
+  ADD UNIQUE KEY `numero` (`numero`),
+  ADD KEY `fk_usuario` (`usuario`),
+  ADD KEY `fkorigen` (`origen`),
+  ADD KEY `fkdestino` (`destino`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -60,7 +73,19 @@ ALTER TABLE `pedidos`
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `numero` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `numero` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fkdestino` FOREIGN KEY (`destino`) REFERENCES `cities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fkorigen` FOREIGN KEY (`origen`) REFERENCES `cities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
