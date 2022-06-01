9456f351d03537842583a18b4687deed023bcc25
Romina <romina.lopez@estudiantes.utec.edu.uy>
2022-05-31 21:51:40 -0300

    ya filtra los cupones por fecha

--- LabPHP/application/models/Cupon_model.php
+++ LabPHP/application/models/Cupon_model.php
@@ -13,7 +13,18 @@ class Cupon_model extends CI_Model {
         foreach($us as $row){
             $idUsuario = $row->id;
         }
-        return $this->db->query("select * from cupones c where c.usado = 0 and c.u_recibe =".$idUsuario)->result();
+        $res = $this->db->query("select * from cupones c where c.usado = 0 and c.u_recibe =".$idUsuario)->result();
+        $cupones = array();
+        if(!empty($res)){
+            foreach($res as $row){
+                //para cada cupon debo transformar el vencimiento a date y compararlo con la fecha actual
+                $vencimiento = strtotime(date_create_from_format("d-m-Y", $row->vencimiento)->format('d-m-Y'));
+                if($vencimiento > strtotime(date("d-m-Y"))){
+                    array_push($cupones, $row);
+                }
+            }
+        }
+        return $cupones;
     }
 
     function usarCupon($idCupon){
