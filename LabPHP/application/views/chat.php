b2bf71594d74b92c3a342d1fbf37428956d4405b
CintiaLeal <cintialeal31m@gmail.com>
2022-06-07 09:58:27 -0300

    Arreglos visuales

--- LabPHP/application/views/chat.php
+++ LabPHP/application/views/chat.php
@@ -1,212 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<header>
-    <?php
-    include ('headerLogueado.php');
-?>
-</header>
-
-
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://code.jquery.com/jquery-git.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<link href='https://css.gg/arrow-up-o.css' rel='stylesheet'>
-<style type="text/css">
-/* Some custom styles to beautify this example */
-
-@import url('https://fonts.googleapis.com/css2?family=Sen&family=Unica+One&display=swap');
-
-.row {
-    margin-top: -0.5rem;
-    margin-left: 0%;
-    height: 640px;
-    background: #ffffff;
-    border: none;
-
-}
-
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<style>
 body {
-    margin: 0;
+    background-color: #f4f7f6;
+    margin-top: 20px;
 }
 
-html {
-    margin: 0;
-}
-
-.div_contenedor {
-    height: 100vh;
-}
-
-.div_centrado {
-    width: 80%;
-    height: 100vh;
-    position: absolute;
-    top: 15%;
-    left: 15%;
-
-}
-
-.col {
-    background: linear-gradient(90deg, #d6d6d6,white);
-    background-color: #d6d6d6;
-    position: relative;
-    overflow: hidden;
-  
-    max-width: 100%;
-    min-height: 430px;
-    padding: 10px 15px;
-    width: 100%;
-    border-color: #d6d6d6;
-    border-top: solid;
-}
-
-.col2 {
-    background-color: #d6d6d6;
-    position: relative;
-    overflow: hidden;
-   
-    border-right: solid;
-    max-width: 100%;
-    min-height: 400px;
-    padding: 10px 15px;
-    width: 15%;
-  
-  
-
-}
-
-header {
-    width: 1300px;
-}
-
-.col3 {
-    z-index: 1;
-    /*  background: linear-gradient(90deg, #389393, white);*/
-    opacity: 80%;
-    position: relative;
-    overflow: hidden;
-    max-width: 100%;
-    height: 100px;
-    padding: 10px 15px;
-    width: 100%;
-    
-    border-color: #d6d6d6;
-}
-
-.col4 {
-    background-color: #d6d6d6;
-    opacity: 80%;
-    height: 100px;
-    padding: 10px 15px;
-    width: 100%;
-    
-  
-    
-}
-
-.cont {
-    display: flex;
-    flex-wrap: wrap;
-}
-
-.contt {
-    display: flex;
-    left: 50%;
-    margin-top: 10px;
-}
-
-.c1p {
-    width: 89%;
-    padding: 1%;
-}
-
-.c2p {
-    width: 50%;
-    padding: 1%;
-}
-
-.c3p {
-    padding: 8px 8px;
-
-    width: 9%;
-}
-
-
-.containerc {
-    border: none;
-    background-color: #f1f1f1;
-    border-radius: 5px;
-
-    margin: 10px 0;
-
-
-}
-
-.containerperfil {
-    border: 1px solid rgba(0, 0, 0, 0.5);
-    background-color: #f1f1f1;
-    
-    width: 90%;
-    border-radius: 5px;
-    padding: 10px;
-    margin: 10px 0;
-}
-
-.darker {
-
-    float: right;
-    background-color: #389393;
-    width: 90%;
-    border-radius: 5px;
-    padding: 10px;
-    margin: 10px 0;
-    border-color: #389393;
-    opacity: 70%;
-}
-
-.darkerv {
-    float: left;
-    background-color: #f5a25d;
-    width: 90%;
-    border-radius: 5px;
-    padding: 10px;
-    margin: 10px 0;
-    border-color: #f5a25d;
-    opacity: 50%;
-}
-
-.enviobtn {
-    background-color: #f5a25d;
-    width: 80%;
-    height: 80%;
-    border-radius: 5px;
-    color: black;
-    border-color: #f5a25d;
-
-}
-
-.perfilbtn {
-    background-color: #f5a25d;
-    width: 100%;
-    height: 100%;
-    border-radius: 5px;
+input {
     font-family: 'Sen', sans-serif;
-    font-size: 16px;
+    font-size: 60px;
     color: black;
-    border-right: black;
-    border-color: #f5a25d;
-    opacity: 50%;
+    display: flex;
+
+    border: none;
+    background-color: transparent;
 }
 
-#global {
-    height: 600px;
+.card {
+    background: #D5D8DC;
+    transition: .5s;
+    border: 0;
+    margin-bottom: 30px;
+    border-radius: .55rem;
+    position: relative;
     width: 100%;
-    overflow-y: scroll;
+    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 10%);
+}
+
+.chat-app .people-list {
+    width: 280px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    padding: 20px;
+    z-index: 7
 }
 
 #global2 {
@@ -215,36 +44,270 @@ header {
     overflow-y: scroll;
 }
 
-.demo {}
+.chat-app .chat {
+    margin-left: 280px;
 
-input {
-    width: 100%;
+    border-left: 1px solid #eaeaea
 }
 
-.inputInfo {
-    font-family: 'Sen', sans-serif;
-    font-size: 50px;
-    color: black;
-    display: flex;
-
-    border: none;
-    background-color: transparent;
+.people-list {
+    -moz-transition: .5s;
+    -o-transition: .5s;
+    -webkit-transition: .5s;
+    transition: .5s;
+    overflow-y: scroll;
 }
 
-.inputEnvio {
-    background-color: #c0c0c0;
-    border: none;
-
+.people-list .chat-list li {
+    padding: 10px 15px;
+    list-style: none;
+    border-radius: 3px
 }
 
-footer {
+.people-list .chat-list li:hover {
+    background: #efefef;
+    cursor: pointer
+}
+
+.people-list .chat-list li.active {
+    background: #efefef
+}
+
+.people-list .chat-list li .name {
+    font-size: 15px
+}
+
+.people-list .chat-list img {
+    width: 45px;
+    border-radius: 50%
+}
+
+.people-list img {
+    float: left;
+    border-radius: 50%
+}
+
+.people-list .about {
+    float: left;
+    padding-left: 8px
+}
+
+.people-list .status {
+    color: #999;
+    font-size: 13px
+}
+
+.chat .chat-header {
+    padding: 15px 20px;
+    border-bottom: 2px solid #f4f7f6
+}
+
+.chat .chat-header img {
+    float: left;
+    border-radius: 40px;
+    width: 40px
+}
+
+.chat .chat-header .chat-about {
+    float: left;
+    padding-left: 10px
+}
+
+.chat .chat-history {
+    padding: 20px;
+    background: #D5D8DC;
+    
+    border-bottom: 2px solid #fff
+}
+
+.chat .chat-history ul {
+    padding: 0
+}
+
+.chat .chat-history ul li {
+    list-style: none;
+    margin-bottom: 30px
+}
+
+.chat .chat-history ul li:last-child {
+    margin-bottom: 0px
+}
+
+.chat .chat-history .message-data {
+    overflow-y: scroll;
+    margin-bottom: 15px
+}
+
+.chat .chat-history .message-data img {
+    border-radius: 40px;
+    width: 40px
+}
+
+.chat .chat-history .message-data-time {
+    color: #434651;
+    padding-left: 6px
+}
+
+.chat .chat-history .message {
+    color: #444;
+    padding: 18px 20px;
+    line-height: 26px;
+    font-size: 16px;
+    border-radius: 7px;
+    display: inline-block;
+    position: relative
+}
+
+.chat .chat-history .message:after {
+    bottom: 100%;
+    left: 7%;
+    border: solid transparent;
+    content: " ";
+    height: 0;
+    width: 0;
     position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 30px;
+    pointer-events: none;
+    border-bottom-color: #fff;
+    border-width: 10px;
+    margin-left: -10px
+}
+
+.chat .chat-history .my-message {
+    margin-top: 20px;
+    width: 60%;
+    background: #efefef
+}
+
+.chat .chat-history .my-message:after {
+    bottom: 100%;
+    left: 30px;
+    border: solid transparent;
+    content: " ";
+    height: 0;
+    width: 0;
+    position: absolute;
+    pointer-events: none;
+    border-bottom-color: #efefef;
+    border-width: 10px;
+    margin-left: -10px
+}
+
+.chat .chat-history .other-message {
+    background: #e8f1f3;
+    text-align: right
+}
+
+.chat .chat-history .other-message:after {
+    border-bottom-color: #e8f1f3;
+    left: 93%;
+
+}
+
+.chat .chat-message {
+
+    padding: 20px
+}
+
+.online,
+.offline,
+.me {
+    margin-top: 20px;
+    margin-right: 2px;
+    font-size: 8px;
+    width: 60%;
+    vertical-align: middle
+}
+
+.online {
+    color: #86c541
+}
+
+.offline {
+    color: #e47297
+}
+
+.me {
+    margin-top: 20px;
+    color: #1d8ecd;
+    width: 60%;
+}
+
+.float-right {
+    float: right;
+    margin-top: 20px;
+    width: 60%;
+}
+
+.clearfix:after {
+    visibility: hidden;
+    display: block;
+    font-size: 0;
+    content: " ";
+    clear: both;
+    height: 0
+}
+
+@media only screen and (max-width: 767px) {
+    .chat-app .people-list {
+        height: 465px;
+        width: 100%;
+        overflow-x: auto;
+        background: #fff;
+        left: -400px;
+        display: none
+    }
+
+    .chat-app .people-list.open {
+        left: 0
+    }
+
+    .chat-app .chat {
+        margin: 0
+    }
+
+    .chat-app .chat .chat-header {
+        border-radius: 0.55rem 0.55rem 0 0
+    }
+
+    .chat-app .chat-history {
+        height: 300px;
+        overflow-x: auto
+    }
+}
+
+@media only screen and (min-width: 768px) and (max-width: 992px) {
+    .chat-app .chat-list {
+        height: 650px;
+        overflow-x: auto
+    }
+
+    .chat-app .chat-history {
+        height: 600px;
+        overflow-x: auto
+    }
+}
+
+@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: landscape) and (-webkit-min-device-pixel-ratio: 1) {
+    .chat-app .chat-list {
+        height: 480px;
+        overflow-x: auto
+    }
+
+    .chat-app .chat-history {
+        height: calc(100vh - 350px);
+        overflow-x: auto
+    }
 }
 </style>
-
+<header>
+    <?php
+    include ('headerLogueado.php');
+?>
+</header>
+<link href='https://css.gg/mail.css' rel='stylesheet'>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-git.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 <link href='https://css.gg/user.css' rel='stylesheet'>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script>
@@ -336,8 +399,9 @@ function buscarChat(nick) {
 
                 if (element.recibio == nick) {
                     var div = document.createElement("div");
+
                     div.setAttribute("id", "div");
-                    div.setAttribute("class", "darker");
+                    div.setAttribute("class", "message other-message float-right");
                     div.textContent = element.contenido;
                     micapa.appendChild(div);
                     $('#global2').scrollTop($('#global2').prop('scrollHeight'));
@@ -345,7 +409,7 @@ function buscarChat(nick) {
                 } else {
                     var div = document.createElement("div");
                     div.setAttribute("id", "div");
-                    div.setAttribute("class", "darkerv");
+                    div.setAttribute("class", "message my-message");
                     div.textContent = element.contenido;
                     micapa.appendChild(div);
                     $('#global2').scrollTop($('#global2').prop('scrollHeight'));
@@ -397,86 +461,78 @@ function enviar() {
      $('#global2').scrollTop($('#global2').prop('scrollHeight'));*/
 }
 </script>
-<link href='https://css.gg/mail.css' rel='stylesheet'>
 
-<body>
+<div class="container">
+    <div class="row clearfix">
+        <div class="col-lg-12">
+            <div class="card chat-app">
+                <div id="plist" class="people-list">
 
+                    <?php foreach ($Perfiles as $row) {?>
+                    <ul class="list-unstyled chat-list mt-2 mb-0">
+                        <li class="clearfix">
 
-    <div class="col2">
-        <div><i class="gg-user" style="width: 30px;"></i></div>
-        <div id="global">
-            <?php foreach ($Perfiles as $row) {?>
-            <div class="containerperfil">
-                <div style="width:70%">
-                    <img src="<?=$row->img;?>" alt="Foto de perfil"
-                        style="width:70px; height:70px; border-radius:70px;"> <a><?=$row->nick;?></a>
-                </div>
-                <div style="width:100%">
-                    <a><?=$row->nombre;?> <?=$row->apellido;?></a>
-                </div>
-                <button class="perfilbtn" name="xPerfil" value="<?=$row->nick;?>" onclick="llamada('<?=$row->nick;?>')">
-                    <i class="gg-mail"></i></button>
-            </div>
-            <?php 
+                            <img src="<?=$row->img;?>" alt="Foto de perfil"
+                                style="width:50px; height:50px; border-radius:50px;">
+                            <div class="about">
+                                <div class="name"><a><?=$row->nick;?></a></div>
+                                <div class="status"> <i class="gg-mail"
+                                        onclick="llamada('<?=$row->nick;?>')"></i><?=$row->nombre;?>
+                                    <?=$row->apellido;?> </div>
+
+                            </div>
+                        </li>
+                        <?php 
             } ?>
-        </div>
-    </div>
-
-    <div class="div_centrado">
-
-        <div class="row">
-            <div class="col3">
-                <div class="cont">
-                    <div class="c1p">
-                        <input type="hidden" id="nickU" name="nickU" disabled="disabled" />
-                        <input type="text" class="inputInfo" id="nombreApellido" name="nombreApellido"
-                            disabled="disabled" />
-                    </div>
-                    <div class="c3p">
-                        <img src="img" id="img" alt="Foto de perfil" style="width:70px; height:70px; border-radius:70px;">
-                    </div>
+                    </ul>
                 </div>
-            </div>
+                <div class="chat">
+                    <div class="chat-header clearfix">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
+                                    <img src="img" id="img" alt="Foto de perfil"
+                                        style="width:50px; height:50px; border-radius:50px;">
+                                </a>
+                                <div class="chat-about">
+                                    <input type="hidden" id="nickU" name="nickU" disabled="disabled" />
+                                    <input type="text" id="nombreApellido" name="nombreApellido" disabled="disabled" />
+                                </div>
+                            </div>
 
-            <div class="col">
-                <div id="global2" name="global2">
-                    <div class="cont">
-                        <div class="spinner-border ocultar"
-                            style="visibility:hidden; width: 15rem; height: 15rem; margin: auto; font-size: 50px"
-                            id="spiner" name="spiner" role="status">
-                            <span class="sr-only"></span>
                         </div>
-                        <br>
-                        <div id="demo" style="width:100%">
-                            <div id="div">
+                    </div>
+                    <div class="chat-history">
+                        <div id="global2" name="global2">
+                            <div class="spinner-border ocultar"
+                                style="visibility:hidden; width: 10rem; height: 10rem; margin: 100px; font-size: 40px"
+                                id="spiner" name="spiner" role="status">
+                                <span class="sr-only"></span>
+                            </div>
+                            <div id="demo" style="width:100%">
+                                <div id="div">
+
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div class="chat-message clearfix">
+                        <div class="input-group mb-0">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fa fa-send" onclick="enviar()"></i></span>
+                            </div>
+                            <input type="text" id="contenidoMensaje" name="contenidoMensaje" class="form-control"
+                                placeholder="Enter text here...">
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div class="col4">
-                <div class="contt">
-
-                    <div class="c1p">
-                        <textarea id="contenidoMensaje" name="contenidoMensaje" rows="2" cols="172"></textarea>
-                    </div>
-                    <div class="c3p">
-                        <button class="enviobtn" onclick="enviar()"><a style="font-size:16px;">Enviar</a></button>
-                    </div>
-                </div>
-            
         </div>
     </div>
-    </div>
-
-    </div>
-</body>
+</div>
 <footer>
     <?php
     include ('footer.php');
 ?>
 
 </footer>
-
-</html>
